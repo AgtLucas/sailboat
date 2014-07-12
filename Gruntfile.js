e3f95368185f90a6aef5e4964b5d4d7c48a9ef7a
Joshua Beitler <joshbeitler@gmail.com>
2014-07-12 11:54:08 -0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,347 @@
+/**
+ * Gruntfile
+ *
+ * More information on using Grunt to work with static assets:
+ * http://gruntjs.com/configuring-tasks
+ */
+
+module.exports = function(grunt) {
+
+  /**
+   * CSS files to inject in order
+   * (uses Grunt-style wildcard/glob/splat expressions)
+   *
+   */
+  var cssFilesToInject = [
+    'styles/**/*.css'
+  ];
+
+
+  /**
+   * Javascript files to inject in order
+   * (uses Grunt-style wildcard/glob/splat expressions)
+   *
+   * To use client-side CoffeeScript, TypeScript, etc., edit the
+   * `sails-linker:devJs` task below for more options.
+   */
+  var jsFilesToInject = [
+
+    // Include vendor scripts.
+    // Use .js extension even if file is .coffee
+    // since coffee files get compiled before injection.
+    'vendor/js/socket.io.js',
+    'vendor/js/sails.io.js',
+
+    //
+    // Put other dependencies here
+    //
+
+    'js/app.js',
+
+    // Include all other scripts
+    'js/**/*.js'
+  ];
+
+
+  /**
+   * Client-side HTML templates are injected using the sources below
+   * The ordering of these templates shouldn't matter.
+   * (uses Grunt-style wildcard/glob/splat expressions)
+   */
+  var templateFilesToInject = [
+    'templates/**/*'
+  ];
+
+
+
+  /////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+  //
+  // Modify code below this line with care.
+  //
+  /////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+
+
+  cssFilesToInject = cssFilesToInject.map(function(path) {
+    return '.tmp/public/' + path;
+  });
+
+  jsFilesToInject = jsFilesToInject.map(function(path) {
+    return '.tmp/public/' + path;
+  });
+
+  templateFilesToInject = templateFilesToInject.map(function(path) {
+    return 'assets/' + path;
+  });
+
+  // Get path to core grunt dependencies from Sails
+  var depsPath = grunt.option('gdsrc') || 'node_modules/sails/node_modules';
+  grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
+  grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
+  grunt.loadTasks(depsPath + '/grunt-contrib-coffee/tasks');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          cwd: './assets',
+          src: ['**/*.!(coffee|jade|scss|sass)'],
+          dest: '.tmp/public'
+        }, {
+          expand: true,
+          cwd: './vendor',
+          src: ['**/*.!(coffee|jade|scss|sass)'],
+          dest: '.tmp/public/vendor'
+        }]
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/public',
+          src: ['**/*'],
+          dest: 'www'
+        }]
+      }
+    },
+
+    clean: {
+      dev: ['.tmp/public/**'],
+      build: ['www']
+    },
+
+    jst: {
+      dev: {
+        files: {
+          '.tmp/public/jst.js': templateFilesToInject
+        }
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['spec/**/*.spec.js']
+      }
+    },
+
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: [{
+          expand: true,
+          cwd: 'assets/styles/',
+          src: ['*.scss', '*.sass'],
+          dest: '.tmp/public/styles/',
+          ext: '.css'
+        }]
+      }
+    },
+
+    coffee: {
+      dev: {
+        options: {
+          bare: false
+        },
+        files: [{
+          expand: true,
+          cwd: 'assets/js/',
+          src: ['**/*.coffee'],
+          dest: '.tmp/public/js/',
+          ext: '.js'
+        }, {
+          expand: true,
+          cwd: 'vendor/',
+          src: ['**/*.coffee'],
+          dest: '.tmp/public/vendor/',
+          ext: '.js'
+        }]
+      }
+    },
+
+    concat: {
+      js: {
+        src: jsFilesToInject,
+        dest: '.tmp/public/concat/production.js'
+      },
+      css: {
+        src: cssFilesToInject,
+        dest: '.tmp/public/concat/production.css'
+      }
+    },
+
+    uglify: {
+      dist: {
+        src: ['.tmp/public/concat/production.js'],
+        dest: '.tmp/public/min/production.js'
+      }
+    },
+
+    cssmin: {
+      dist: {
+        src: ['.tmp/public/concat/production.css'],
+        dest: '.tmp/public/min/production.css'
+      }
+    },
+
+    'sails-linker': {
+      devJs: {
+        options: {
+          startTag: '// SCRIPTS',
+          endTag: '// SCRIPTS END',
+          fileTmpl: 'script(type="text/javascript", src="%s")',
+          appRoot: '.tmp/public'
+        },
+        files: {
+          'js': jsFilesToInject
+        }
+      },
+
+      prodJs: {
+        options: {
+          startTag: '// SCRIPTS',
+          endTag: '// SCRIPTS END',
+          fileTmpl: 'script(type="text/javascript", src="%s")',
+          appRoot: '.tmp/public'
+        },
+        files: {
+          'js': ['.tmp/public/min/production.js']
+        }
+      },
+
+      devStyles: {
+        options: {
+          startTag: '// STYLES',
+          endTag: '// STYLES END',
+          fileTmpl: 'link(rel="stylesheet", href="%s")',
+          appRoot: '.tmp/public'
+        },
+        files: {
+          'styles': cssFilesToInject
+        }
+      },
+
+      prodStyles: {
+        options: {
+          startTag: '// STYLES',
+          endTag: '// STYLES END',
+          fileTmpl: 'link(rel="stylesheet", href="%s")',
+          appRoot: '.tmp/public'
+        },
+        files: {
+          'styles': ['.tmp/public/min/production.css']
+        }
+      },
+
+      devTpl: {
+        options: {
+          startTag: '// TEMPLATES',
+          endTag: '// TEMPLATES END',
+          fileTmpl: 'script(type="text/javascript", src="%s")',
+          appRoot: '.tmp/public'
+        },
+        files: {
+          'tpl': ['.tmp/public/jst.js']
+        }
+      }
+    },
+
+    watch: {
+      api: {
+        // API files to watch:
+        files: ['api/**/*']
+      },
+      assets: {
+        // Assets to watch:
+        files: ['assets/**/*', 'vendor/**/*'],
+        // When assets are changed:
+        tasks: ['compileAssets', 'linkAssets']
+      }
+    }
+  });
+
+  // When Sails is lifted:
+  grunt.registerTask('default', [
+    'compileAssets',
+    'linkAssets',
+    'watch'
+  ]);
+
+  grunt.registerTask('test', ['mochaTest']);
+
+  grunt.registerTask('compileAssets', [
+    'clean:dev',
+    'jst:dev',
+    'sass:dev',
+    'copy:dev',
+    'coffee:dev'
+  ]);
+
+  grunt.registerTask('linkAssets', [
+    // Uncomment to automatically include css files
+    //'sails-linker:devStyles',
+    'sails-linker:devJs',
+    'sails-linker:devTpl'
+  ]);
+
+  // Build the assets into a web accessible folder.
+  // (handy for phone gap apps, chrome extensions, etc.)
+  grunt.registerTask('build', [
+    'compileAssets',
+    'linkAssets',
+    'clean:build',
+    'copy:build'
+  ]);
+
+  // When sails is lifted in production
+  grunt.registerTask('prod', [
+    'clean:dev',
+    'jst:dev',
+    'sass:dev',
+    'copy:dev',
+    'coffee:dev',
+    'concat',
+    'uglify',
+    'cssmin',
+    'sails-linker:prodJs',
+    'sails-linker:prodStyles',
+    'sails-linker:devTpl',
+    'sails-linker:prodJs',
+    'sails-linker:prodStyles',
+    'sails-linker:devTpl'
+  ]);
+
+  // When API files are changed:
+  // grunt.event.on('watch', function(action, filepath) {
+  //   grunt.log.writeln(filepath + ' has ' + action);
+
+  //   // Send a request to a development-only endpoint on the server
+  //   // which will reuptake the file that was changed.
+  //   var baseurl = grunt.option('baseurl');
+  //   var gruntSignalRoute = grunt.option('signalpath');
+  //   var url = baseurl + gruntSignalRoute + '?action=' + action + '&filepath=' + filepath;
+
+  //   require('http').get(url)
+  //   .on('error', function(e) {
+  //     console.error(filepath + ' has ' + action + ', but could not signal the Sails.js server: ' + e.message);
+  //   });
+  // });
+};
---
+++ README.md
@@ -0,0 +1,36 @@
+# sailboat
+
+A handy-dandy pre-made [Sails](http://sailsjs.org) application starter kit with
+sensible Rails-like defaults.
+
+### Getting Started
+You can get started building apps with Sailboat by simply cloning the repo:
+```
+$ git clone https://github.com/joshbeitler/sailboat <myapp>
+$ cd <myapp> && sails lift
+```
+
+### Features
+- Unit testing via Mocha
+- Write your app with CoffeeScript, Jade, Sass
+- Flexible authentication via Passport and passport-oauth
+- API key generation and two factor authentication with speakeasy
+- Foundation 5, normalize.css, modernizr, jQuery
+- FontAwesome
+
+### License
+```
+Copyright 2014 Joshua Beitler
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
